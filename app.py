c3c3f1be69223b3cd9871e7efc365bf509f44e9d
RehanDias <41712284+RehanDias@users.noreply.github.com>
2024-03-03 21:07:18 +0700

    Update app.py to add error handling

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 import json
-import urllib.parse
 import urllib.request
 import time
-
 
 # Variabel konfigurasi
 # Example url Detail to get botToken, chatId
 # https://api.telegram.org/bot6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ/sendMessage?parse_mode=markdown&chat_id=6465082908&text=hallo
-
 config = {
-   "botToken": "6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ",
-   "chatId": "6465082908",
-   "message": "hallo",
-   "interval": 5,
-   "requestRetryInterval": 30,
-   "numberOfRequests": 10,
+    "botToken": "6680557007:AAGmHtzlEVGXJxKxxF62tUPFx9kmYeqk5QQ",  # Token bot Telegram
+    "chatId": "6465082908",  # ID obrolan untuk mengirim pesan
+    "message": "hallo",  # Pesan yang akan dikirim
+    "interval": 5,  # Interval waktu dalam detik antara setiap pengiriman pesan
+    "requestRetryInterval": 30,  # Interval waktu dalam detik sebelum mencoba lagi setelah mendapatkan kesalahan 429 (Batas permintaan tercapai)
+    "numberOfRequests": 10,  # Jumlah total permintaan yang akan dikirim
 }
 
 # Fungsi untuk mengirim pesan menggunakan urllib
@@ -29,27 +26,37 @@ def send_message(text):
         with urllib.request.urlopen(req) as response:
             response_data = json.loads(response.read().decode("utf-8"))
             if response_data["ok"]:
-                print(f"Pesan '{text}' berhasil dikirim: Berhasil melakukan SPAM")
+                print(f"Pesan '{text}' berhasil dikirim")
             else:
                 print("Gagal mengirim pesan:", response_data["description"])
-    except Exception as e:
-        handle_error(e)
+    except urllib.error.HTTPError as e:
+        handle_http_error(e)
+    except urllib.error.URLError as e:
+        handle_url_error(e)
 
 # Fungsi utama untuk mengirim pesan secara terus menerus dengan interval
 def send_messages_continuously():
     count = 0
     while count < config["numberOfRequests"]:
-        send_message(config["message"])
-        time.sleep(config["interval"])
-        count += 1
+        try:
+            send_message(config["message"])
+            time.sleep(config["interval"])
+            count += 1
+        except Exception as e:
+            print("Terjadi kesalahan:", e)
+            time.sleep(config["requestRetryInterval"])
 
-# Fungsi untuk menangani kesalahan
-def handle_error(error):
-    print("Error:", error)
-    if hasattr(error, "response") and error.response.status_code == 429:
+# Fungsi untuk menangani kesalahan HTTP
+def handle_http_error(error):
+    print("Error HTTP:", error)
+    if error.code == 429:
         print("Batasan permintaan API telah tercapai. Menunggu...")
         time.sleep(config["requestRetryInterval"])
         send_messages_continuously()
+
+# Fungsi untuk menangani kesalahan URL
+def handle_url_error(error):
+    print("Error URL:", error)
 
 # Panggil fungsi send_messages_continuously untuk memulai pengiriman pesan terus menerus
 send_messages_continuously()
