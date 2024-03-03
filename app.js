02a11c1feb6cf31bf42973f2bcfd2703aff146d2
RehanDias <41712284+RehanDias@users.noreply.github.com>
2024-03-03 20:34:41 +0700

    The app.js update adds details of what message is sent when successful

--- app.js
+++ app.js
@@ -11,42 +11,43 @@ const config = {
 
 // Fungsi untuk mengirim pesan menggunakan axios
 async function sendMessage(text) {
-   const url = `https://api.telegram.org/bot${config.botToken}/sendMessage`;
-   try {
-      await axios.post(url, {
-         chat_id: config.chatId,
-         text: text,
-      });
-      console.log("Berhasil melakukan SPAM");
-   } catch (error) {
-      handleError(error);
-   }
+    const url = `https://api.telegram.org/bot${config.botToken}/sendMessage`;
+    try {
+        const response = await axios.post(url, {
+            chat_id: config.chatId,
+            text: text
+        });
+        console.log(`Pesan "${text}" berhasil dikirim: Berhasil melakukan SPAM`);
+    } catch (error) {
+        handleError(error);
+    }
 }
 
 // Fungsi utama untuk mengirim pesan secara terus menerus dengan interval
 async function sendMessagesContinuously() {
-   while (true) {
-      await sendMessage(config.message);
-      await sleep(config.interval);
-   }
+    while (true) {
+        await sendMessage(config.message);
+        await sleep(config.interval);
+    }
 }
 
 // Fungsi untuk menangani kesalahan
 function handleError(error) {
-   console.error("Error:", error.message);
-   if (error.response && error.response.status === 429) {
-      console.log("Batasan permintaan API telah tercapai. Menunggu...");
-      setTimeout(sendMessagesContinuously, config.requestRetryInterval);
-   }
+    console.error("Error:", error.message);
+    if (error.response && error.response.status === 429) {
+        console.log("Batasan permintaan API telah tercapai. Menunggu...");
+        setTimeout(sendMessagesContinuously, config.requestRetryInterval);
+    }
 }
 
 // Fungsi untuk menunda eksekusi dalam milidetik
 function sleep(ms) {
-   return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // Panggil fungsi sendMessagesContinuously untuk memulai pengiriman pesan terus menerus
-sendMessagesContinuously().catch((error) => {
-   console.error("Terjadi kesalahan fatal:", error.message);
-   process.exit(1);
-});
+sendMessagesContinuously()
+    .catch(error => {
+        console.error("Terjadi kesalahan fatal:", error.message);
+        process.exit(1);
+    });
